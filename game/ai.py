9d7a8a23d4dd06fb0019377db0c85db7aa932bca
AdvancedEel850 <90942089+AdvancedEel850@users.noreply.github.com>
2021-12-02 10:18:54 -0700

    update

--- game/ai.py
+++ game/ai.py
@@ -14,21 +14,19 @@ class Aritficial(ATAT):
         self.ghost = ghost
 
         for i in range(0,len(self.ghost)):
-            if self.ghost[i].velocity == (0,0):
-                self.ghost[i].velocity = (random.randint(-1,1), 0)
+            self.atat = self.ghost[i]
+            if self.atat.velocity == (0,0):
+                self.atat.velocity = (random.randint(-1,1), 0)
     
     def check_collision(self):
         
-        for i in range(len(self.ghost)):
-            if self.ghost[i].collides_with_list(self.walls):
-                if self.ghost[i].velocity == (0,1):
-                    self.ghost[i].velocity = (0,-2)
-                    sleep(.5)
-                    self.ghost[i].velocity = (0,0)
-                elif self.ghost[i].velocity == (0,-1):
-                    self.ghost[i].velocity = (0,2)
-                    sleep(.5)
-                    self.ghost[i].velocity = (0,0)
+        for i in range(0,len(self.ghost)):
+            self.atat = self.ghost[i]
+            if self.atat.collides_with_list(self.walls):
+                if self.atat.velocity == (0,1):
+                    self.atat.velocity = (0,0)
+                elif self.atat.velocity == (0,-1):
+                    self.atat.velocity = (0,0)
                 elif self.ghost[i].velocity == (1,0):
                     self.ghost[i].velocity = (-2,0)
                     sleep(.5)
