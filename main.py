b53f502d4747451cdab26499a14dfa921b0bffce
AdvancedEel850 <90942089+AdvancedEel850@users.noreply.github.com>
2021-12-09 14:14:05 -0700

    finished

--- main.py
+++ main.py
@@ -93,11 +93,13 @@ class My_Game(arcade.Window):
 
         if len(self.dots) == 0:
             for n in range(0,len(self.ghost)):
-                self.ghost[n].remove_from_sprite_lists()
+                atat = self.ghost[n]
+                atat.remove_from_sprite_lists()
                 self.ghost.update()
-                sleep(1)
+                sleep(.5)
             self.player.remove_from_sprite_lists()
             self.all_sprites.update()
+            sleep(.5)
             arcade.finish_render()
             sleep(2)
             arcade.close_window()
